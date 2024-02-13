467aa1963283dfeafdcbcd8c090738eaef9b5f49
Yash Chawla <yashchawla2000@gmail.com>
2024-02-12 19:18:33 -0500

    Changes: Adding to previous Commit
    ToDo:
    npm install bootstrap@v5.3.2

--- owl-judge/src/components/Dashboard.js
+++ owl-judge/src/components/Dashboard.js
@@ -3,14 +3,13 @@ import "./Dashboard.css";
 
 function Dashboard() {
   return (
-      <div className="dashboard">
-        <h1>Welcome to Owl Judge</h1>
-        <p>
-          Your one-stop platform for managing and judging capstone projects
-          efficiently.
-        </p>
-      </div>
-
+    <div className="dashboard">
+      <h1>Welcome to Owl Judge</h1>
+      <p>
+        Your one-stop platform for managing and judging capstone projects
+        efficiently.
+      </p>
+    </div>
   );
 }
 
--- owl-judge/src/components/Navigation.js
+++ owl-judge/src/components/Navigation.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Navigation.css'; 
+import ksuLogo from './Images/KSU-Logo.png'; 
 
 function Navigation() {
   return (
     <nav>
       <ul>
+        <li className="logo-item">
+          <img src={ksuLogo} alt="KSU Logo" className="ksu-logo" />
+        </li>
         <li><Link to="/">Dashboard</Link></li>
-        <li><Link to="/event-form">Create Event</Link></li>
+        <li><Link to="/event-form">Create Event</Link></li> 
       </ul>
     </nav>
   );
